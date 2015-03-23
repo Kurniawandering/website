2a6b29239dba436b8335b5d47e0e6e03a1776df9
Hendrik Kurniawan <hendrik@strangelove.nl>
2015-03-23 01:59:38 +0100

    hotfix redirect api

--- public/src/scripts/essays/spotify/config.js
+++ public/src/scripts/essays/spotify/config.js
@@ -5,7 +5,7 @@ var config = {
 	CLIENT_ID		: 'a50a0f1aa12c4bafa7e12762cafaddfa',
     
     //REDIRECT_URI	: 'http://localhost:3000/essays/on-spotify'
-    REDIRECT_URI	: 'http://kubrick.io/essays/on-spotify'
+    REDIRECT_URI	: 'http://www.kubrick.io/essays/on-spotify'
 };
 
 module.exports = config;
